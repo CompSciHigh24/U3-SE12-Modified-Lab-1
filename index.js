a2aa3ce9e2f9dc1ec9a6534eeccd94c924ce21c7
CompSciHigh24 <csh-github@compscihigh.org>
2025-04-10 13:40:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Step 1: Install Express.js via the terminal command:
-// 'npm install express' or the shorthand version 'npm i express'
+// Step 1: Install Express via the terminal command: npm i express
 
 // Step 2: Import the express module using 'require', name the variable express
 
@@ -7,14 +6,8 @@
 
 // Step 4: Set up the server using 'app.listen'. 
 // Set the port is set to 3000. 
-// Leave the callback function empty for now.
 
-// Step 5: Start your server by running the index.js file
-// Notice how the terminal is running but nothing is being console logged. 
-// Stop your server by using the command ctrl + c
+// Step 5: Console log "Server is running!" inside the callback function. 
 
-// Step 6: Console log "Server is running at http://localhost:3000" inside the callback function. 
-// Restart your server
-
-// Step 7: Follow the local host URL. 
+// Step 6: Open the server.
 
